530d1209c24f2562d8387c3d301c2eb40ca87eb0
heatherhop <heatherhop77@gmail.com>
2025-07-07 15:52:35 -0600

    added a comment for practice

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        //Ask the user for their name.
         Console.Write("What is your first name? ");
         string firstName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
         string lastName = Console.ReadLine();
-        
+
         Console.WriteLine($"My name is {lastName}, {firstName} {lastName}.");
-        
+
     }
 }
